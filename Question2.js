ea965451a1fbf3830ad008b2c32f61f5a1c4b49b
Navdeep Singh <navdeep@binmile.com>
2022-03-25 15:27:30 +0530

    Updated

--- Question2.js
+++ Question2.js
@@ -1,11 +1,37 @@
-const a = [1,2,3,4];
-const b = [8,7,6,5];
+const a = [3,10,40,45,50,60,69];
+const b = [60,50,40,20,1];
 
+const getCombinedArray = (a,b) => {
+    let resultArray = [];
+    const mainlength = a.length+b.length;
+    let j=0;
+    let h=b.length-1; 
+    for(i=0;i<mainlength;i++)
+    {
+        if(a[j]<b[h])
+            {
+                resultArray.push(a[j]);
+                j+=1;
+            }
+            else{
+                resultArray.push(b[h]);
+                h-=1;
+            }
+            if(h<0)
+            {
+                resultArray= [...resultArray,...(a.slice(j))];
+                return resultArray;
+            }
+        
+    }
+    return resultArray;
+}
 const getMedian = (a,b) => {
     //validate
     if(a&&b&&Array.isArray(a)&&Array.isArray(b))
     {
-        const combinedArray = [...a,...b].sort((a,b) => b-a);
+        const combinedArray = getCombinedArray(a,b);
+        console.log(combinedArray);
         if(combinedArray.length%2 == 0)
         {
             return (combinedArray[combinedArray.length/2]+combinedArray[(combinedArray.length/2)-1])/2;
